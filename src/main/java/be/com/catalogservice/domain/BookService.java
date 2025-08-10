21b798e83207e3f92db1279e93713f9c16de5de9
behappyleee <72903623+behappyleee@users.noreply.github.com>
2025-08-11 00:36:19 +0900

    feat: add book service feature

---
+++ src/main/java/be/com/catalogservice/domain/BookService.java
@@ -0,0 +1,26 @@
+package be.com.catalogservice.domain;
+
+import org.springframework.stereotype.Service;
+
+import java.util.NoSuchElementException;
+
+@Service
+public class BookService {
+
+    private final BookRepository bookRepository;
+
+    public BookService(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    public Iterable<Book> viewBookList() {
+        return bookRepository.findAll();
+    }
+
+    public Book viewBookDetails(String isbn) {
+        return bookRepository.findByIsbn(isbn)
+                .orElseThrow(() -> new NoSuchElementException(String.format("Book with isbn %s not found", isbn)));
+    }
+
+}
+
--- src/main/java/be/com/catalogservice/service/BookService.java
+++
@@ -1,9 +0,0 @@
-package be.com.catalogservice.service;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class BookService {
-
-}
-
