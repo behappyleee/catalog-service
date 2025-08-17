66f9d7f9c09a19896b7633b7c7986ecef4a5c6ca
behappyleee <72903623+behappyleee@users.noreply.github.com>
2025-08-17 17:54:33 +0900

    feat: add testdata

---
+++ src/main/java/be/com/catalogservice/demo/BookDataLoader.java
@@ -0,0 +1,28 @@
+package be.com.catalogservice.demo;
+
+import be.com.catalogservice.domain.Book;
+import be.com.catalogservice.domain.BookRepository;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.annotation.Profile;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@Profile("testdata")
+public class BookDataLoader {
+
+    private final BookRepository bookRepository;
+
+    public BookDataLoader(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void loadBookTestData() {
+        var book1 = new Book("1234567891", "Northern Lights", "Lyra Siverstar", 9.90);
+        var book2 = new Book("1234567892", "Polar Journey", "Iorek  Plarson", 12.90);
+
+        bookRepository.save(book1);
+        bookRepository.save(book2);
+    }
+}
