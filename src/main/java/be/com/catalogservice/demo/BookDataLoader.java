dc08faf2f03cfd090066a7499b6143c122a2bce9
behappyleee <72903623+behappyleee@users.noreply.github.com>
2025-08-26 01:25:28 +0900

    feat: change repository methods

--- src/main/java/be/com/catalogservice/demo/BookDataLoader.java
+++ src/main/java/be/com/catalogservice/demo/BookDataLoader.java
@@ -7,6 +7,8 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @Component
 @Profile("testdata")
 public class BookDataLoader {
@@ -21,7 +23,6 @@ public class BookDataLoader {
         var book1 = Book.of("1234567891", "Northern Lights", "Lyra Silverstar", 9.90);
         var book2 = Book.of("1234567892", "Polar Journey", "Iorek Ploarson", 12.90);
 
-        bookRepository.save(book1);
-        bookRepository.save(book2);
+        bookRepository.saveAll(List.of(book1, book2));
     }
 }
